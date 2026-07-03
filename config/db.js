2b415d13e1c7a7494a0e9d64ad5ed8232832ccbb
Gadielyn Siahaan <gadielynsiahaan@student.esaunggul.ac.id>
2026-07-03 23:22:00 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,35 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-
-    host:"localhost",
-
-    user:"root",
-
-    password:"root12345",
-
-    database:"gekisia"
-
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
-db.connect((err)=>{
-
-    if(err){
-
-        console.log(err);
-
-    }else{
-
+db.connect((err) => {
+    if (err) {
+        console.log("Database Error:", err);
+    } else {
         console.log("MySQL Connected");
-
-        db.query("SELECT DATABASE() AS db",(e,r)=>{
-
-            console.log(r);
-
-        });
-
     }
-
 });
 
-module.exports=db;
+module.exports = db;
