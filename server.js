3fdc75c642c80a9ebb086f3c63046fabcc4ba013
Gadielyn Siahaan <gadielynsiahaan@student.esaunggul.ac.id>
2026-07-03 23:22:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -59,7 +61,7 @@ app.use((err, req, res, next) => {
     next();
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
