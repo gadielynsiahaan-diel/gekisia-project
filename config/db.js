72c2ffb6167a3fbf572e2d25d050006f8cf02e0f
Gadielyn Siahaan <gadielynsiahaan@student.esaunggul.ac.id>
2026-07-04 11:11:11 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,17 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   ssl: {
     rejectUnauthorized: false,
   },
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("Database Error:", err);
-  } else {
-    console.log("TiDB Connected");
-  }
 });
 
 module.exports = db;
