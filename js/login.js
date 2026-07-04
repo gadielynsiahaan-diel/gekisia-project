5bfdffd2bb6f3455c4c8e503cfd98142b9877c0c
Gadielyn Siahaan <gadielynsiahaan@student.esaunggul.ac.id>
2026-07-04 07:52:04 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -27,7 +27,7 @@ form.addEventListener("submit", async (e) => {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/login", {
+        const response = await fetch("https://gekisia-project-production.up.railway.app/api/login", {
 
             method: "POST",
 
